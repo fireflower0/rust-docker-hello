07b27dbba2b217d4261a7bffe44b40486906de58
fireflower0 <fireflower0@hotmail.co.jp>
2021-03-21 16:53:37 +0900

    Fixed a src

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
-use actix_web::{get, App, HttpServer};
+use actix_web::{get, web, App, HttpServer, Responder};
 
 #[get("/")]
-async fn index() -> &'static str {
-    "Hello world!\r\n"
+async fn index() -> impl Responder {
+    "Hello, world!"
+}
+
+#[get("/{name}")]
+async fn hello(web::Path(name): web::Path<String>) -> impl Responder {
+    format!("Hello, {}!", name)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(index)
-    }).bind("0.0.0.0:8080")?.run().await
+        App::new()
+            .service(index)
+            .service(hello)
+    }).bind("0.0.0.0:8080")?
+        .run()
+        .await
 }
